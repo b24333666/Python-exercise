17935c2d4baa1879846f13f283b719c167a0f81b
b24333666 <34462790+b24333666@users.noreply.github.com>
2017-12-12 18:28:52 +0800

    2017/12/12

---
+++ test.py
@@ -0,0 +1,16 @@
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Michelle', 'Ola', 'Mosky', 'Phoebe', 'you']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+if 2 > 5:
+    print('2 > 5')
+elif 2 < 5:
+    print('2 < 5')
+else:
+    print('2 == 5')
+
