8615f6abff9e33f8f154d2dbf401dbb403b55667
b24333666 <b24333666@hotmail.com>
2018-01-03 18:53:24 +0800

    01/03

--- bank/tuptest.py
+++ bank/tuptest.py
@@ -1,6 +1,9 @@
 from collections import namedtuple
+import time
+# parts = {'bill':'wid','taol':'long'}
+# duck = Duck(**parts)
 
-parts = {'bill':'wid','taol':'long'}
-duck = Duck(**parts)
+# print(duck)
 
-print(duck)
+dateName = time.strftime("%Y_%M_%D")
+print(dateName) 
