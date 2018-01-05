71b42a8c4f0262bc4d2253a14a986762553e4dd2
b24333666 <b24333666@hotmail.com>
2018-01-05 17:13:59 +0800

    1/5

--- PM2.5/auto_1.py
+++ PM2.5/auto_1.py
@@ -4,7 +4,7 @@ import os
 
 md5 = hashlib.md5()
 url = 'https://quality.data.gov.tw/dq_download_csv.php?nid=34827&md5_url=1971e5ea258e4e5e2a2ad57e6330a603'
-md5.update(b'Test string!')
+# md5.update(b'Test string!')
 print(md5.hexdigest())
 
 html = requests.get(url).text.encode('UTF-8-sig')
@@ -20,7 +20,7 @@ else:
     with open('old_md5.txt', 'w') as f:
         f.write(md5)
 
-if md5 != old_md5:
+if md5 == old_md5:
     print("資料已更新...")
 else:
     print("資料未更新，正在從資料庫讀取...")
