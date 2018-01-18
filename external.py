4d4e2a531b050f7d3b58d0f6075c43127d24271a
b24333666 <b24333666@hotmail.com>
2018-01-18 22:56:38 +0800

    1/18

---
+++ external.py
@@ -0,0 +1,73 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+from urllib.parse import urlparse
+
+
+pages = set()
+random.seed(datetime.datetime.now())
+
+def getinternallinks(bsobj,includeurl):
+    internallinks = []
+
+    for link in bsobj.findAll("a",href=re.compile("^(/|.*" + includeurl + ")")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internallinks:
+                internallinks.append(link.attrs['href'])
+    return internallinks
+
+def getexternallinks(bsobj,excludeurl):
+    externallinks = []
+
+    for link in bsobj.findAll("a",href=re.compile("^(http|WWW)((?!" + excludeurl + ").)*$")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externallinks:
+                externallinks.append(link.attrs['href'])
+    return externallinks
+
+def splitaddres(addres):
+    addresparts = addres.replace("http://","").split("/")
+    return addresparts
+
+def getrandomexternallinks(startingpage):
+    html = urlopen(startingpage)
+    bsobj = BeautifulSoup(html)
+    externallinks = getexternallinks(bsobj,urlparse(startingpage).netloc)
+    if len(externallinks) == 0:
+        print("")
+        domain = urlparse(startingpage).scheme + "://" + urlparse(startingpage).netloc
+        internallinks = getinternallinks(bsobj,domain)
+        return getrandomexternallinks(internallinks[random.randint(0,len(internallinks)-1)])
+
+    else:
+        return externallinks[random.randint(0,len(externallinks)-1)]
+
+def followexternalomly(startingsite):
+    externallink = getrandomexternallinks("http://oreilly.com")
+    print("random external link is: "+ externallink)
+    followexternalomly(externallink)
+
+followexternalomly("http://oreilly.com")
+
+
+allexternallinks = set()
+allinternallinks = set()
+
+def getallexternallinks(siteurl):
+    html = urlopen(siteurl)
+    bsobj = BeautifulSoup(html)
+    internallinks = getinternallinks(bsobj.splitaddress(siteurl)[0])
+    externallinks = getexternallinks(bsobj.splitaddress(siteurl)[0])
+    for link in externallinks:
+        if link not in externallinks:
+            allexternallinks.add(link)
+            print(link)
+    for link in internallinks:
+        if link not in allinternallinks:
+            print("abuot to get link: " + link)
+            allinternallinks.add(link)
+            getallexternallinks(link)
+
+getallexternallinks("http://oreilly.com")
